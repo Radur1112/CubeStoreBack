1562a88921e2afac9d596a501b394c5debb62ac9
Radur1112 <123523316+Radur1112@users.noreply.github.com>
2024-05-16 13:27:43 -0600

    Delete model directory

--- model/tipoUsuarios.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const tipoUsuarioSchema = mongoose.Schema({
-    descripcion: String
-});
-
-const tipoUsuarioModel = mongoose.model('TipoUsuarios', tipoUsuarioSchema);
-
-module.exports = tipoUsuarioModel;
